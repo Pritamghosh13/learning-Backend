59004fd01d82fa94f2cf0e1f5f6b10b62f579308
Pritam <ghoshpritam0708@gmail.com>
2026-05-15 18:22:55 +0530

    adding update and delete tweet features, along with fetching user tweets

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -31,9 +31,98 @@ const createTweet = asyncHandler(async (req, res) => {
 
 //update tweet
 const updateTweet = asyncHandler(async (req, res) => {
-    cont 
+    const {content} = req.body;
+    const {tweetId} = req.params;
+
+    if (!content || content.trim() === "") {
+        throw new ApiError(400, "The content of tweet is required")
+    }
+
+    const tweet = await Tweet.findOneAndUpdate(
+        {
+            _id: tweetId,
+            owner: req.user?._id
+        },
+        {
+            $set: {
+                content
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!tweet){
+        throw new ApiError(404, "Tweet failed to update")
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(200, tweet, "Tweet update successfully"))
+
+})
+
+//delete tweet
+const deleteTweet = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params;
+
+    const tweet = await Tweet.deleteOne({
+        _id: tweetId,
+        owner: req.user?._id
+    })
+
+    if (tweet.deletedCount === 0) {
+    throw new ApiError(404, "Tweet not found or unauthorized");
+    }
+
+
+    return res.status(200)
+    .json(new ApiResponse(200, {}, "Your tweet deleted successfully"))
+})
+
+
+//get all users tweet
+const getTweets = asyncHandler(async (req, res) => {
+    const {userId} = req.params;
+    let {page = 1, limit = 10} = req.query;
+
+    page = Number(page);
+    limit = Number(limit);
+
+    if (!userId) {
+        throw new ApiError(400, "User ID is required");
+    }
+
+    const skip = (page - 1) * limit;
+
+    const tweets = await Tweet.find({
+        owner: userId
+    }).skip(skip)
+    .limit(limit)
+    .populate("owner", "username, fullname")
+    .sort({ createdAt: -1 })
+
+
+    const totalTweet = await Tweet.countDocuments({
+        owner: userId
+    })
+
+    return res.status(200)
+    .json({
+        suceess: true,
+        totalTweet,
+        tweets,
+        limit,
+        totalPages : Math.ceil(totalTweet/limit),
+        message: "User tweets fetched successfully"
+    }
+        
+    )
 })
 
 export {
-    createTweet
+    createTweet,
+    updateTweet,
+    deleteTweet,
+    getTweets
 }
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createTweet } from "../controllers/tweet.controller.js";
+import { createTweet, updateTweet, deleteTweet, getTweets } from "../controllers/tweet.controller.js";
 
 
 const router = Router()
@@ -8,6 +8,11 @@ const router = Router()
 
 router.route("/add").post(verifyJWT, createTweet)
 
+router.route("/update/:tweetId").patch(verifyJWT, updateTweet)
+
+router.route("/delete/:tweetId").delete(verifyJWT, deleteTweet)
+
+router.route("/getall/:userId").get(verifyJWT, getTweets)
 
 
 
